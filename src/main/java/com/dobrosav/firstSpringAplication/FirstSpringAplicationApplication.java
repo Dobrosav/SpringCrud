ee7dc9de64aaff7ae1b9e4074d4710555e2d2bdb
Dobrosav Vlaskovic <vlaskovic.dodo@gmail.com>
2022-11-16 19:31:38 +0100

    init commit

---
+++ src/main/java/com/dobrosav/firstSpringAplication/FirstSpringAplicationApplication.java
@@ -0,0 +1,13 @@
+package com.dobrosav.firstSpringAplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringAplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSpringAplicationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dobrosav/firstSpringAplication/FirstSpringAplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dobrosav.firstSpringAplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringAplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
