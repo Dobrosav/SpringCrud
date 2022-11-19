1b796dc40982e1e36469ccb9640bcdf5b85fcd60
Dobrosav Vlaskovic <vlaskovic.dodo@gmail.com>
2022-11-19 13:13:05 +0100

    Getting and deleting by id

--- src/main/java/com/dobrosav/firstSpringAplication/controllers/BlogController.java
+++ src/main/java/com/dobrosav/firstSpringAplication/controllers/BlogController.java
@@ -3,7 +3,9 @@ package com.dobrosav.firstSpringAplication.controllers;
 import com.dobrosav.firstSpringAplication.models.Blog;
 import com.dobrosav.firstSpringAplication.models.BlogReprository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -12,9 +14,23 @@ import java.util.List;
 public class BlogController {
     @Autowired
     BlogReprository blogReprository;
+    private int blogId;
 
     @GetMapping("/blog")
     public List<Blog> index(){
         return this.blogReprository.findAll();
     }
+    @GetMapping("/blog/{id}")
+    public Blog getBlogById(@PathVariable String id){
+        int blogId = Integer.parseInt(id);
+        return this.blogReprository.findById(blogId).get();
+    }
+    @DeleteMapping("/blog/{id}")
+    public String deleteById(@PathVariable String id){
+        int blogId = Integer.parseInt(id);
+        this.blogReprository.deleteById(blogId);
+        return "DELETED";
+    }
+
+
 }
