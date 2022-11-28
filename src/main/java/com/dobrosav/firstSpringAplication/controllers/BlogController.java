fa85ba4d578e6431f38dadc7fee6410adcc8877b
Dobrosav Vlaskovic <vlaskovic.dodo@gmail.com>
2022-11-28 10:35:07 +0100

    Code reformatted

--- src/main/java/com/dobrosav/firstSpringAplication/controllers/BlogController.java
+++ src/main/java/com/dobrosav/firstSpringAplication/controllers/BlogController.java
@@ -16,58 +16,62 @@ import java.util.NoSuchElementException;
 public class BlogController {
 
     BlogReprository blogReprository;
+
     @Autowired
-    public BlogController(BlogReprository blogReprository){
-        this.blogReprository=blogReprository;
+    public BlogController(BlogReprository blogReprository) {
+        this.blogReprository = blogReprository;
     }
+
     private static Message message = new Message();
+
     @GetMapping("/blog")
-    public List<Blog> index(){
+    public List<Blog> index() {
         return this.blogReprository.findAll();
     }
+
     @GetMapping("/blog/{id}")
     @ResponseBody
-    public ResponseEntity getBlogById(@PathVariable String id){
+    public ResponseEntity getBlogById(@PathVariable String id) {
         try {
             int blogId = Integer.parseInt(id);
             return new ResponseEntity(this.blogReprository.findById(blogId).get(), HttpStatus.OK);
-        }
-        catch (NoSuchElementException exception){
+        } catch (NoSuchElementException exception) {
             message.setText("Not found");
             return new ResponseEntity(message, HttpStatus.NOT_FOUND);
         }
     }
+
     @GetMapping("/blogWithId")
     @ResponseBody
-    public ResponseEntity getBlogByIdFromParameters(@RequestParam(value = "id") int id){
+    public ResponseEntity getBlogByIdFromParameters(@RequestParam(value = "id") int id) {
         try {
             return new ResponseEntity(this.blogReprository.findById(id).get(), HttpStatus.OK);
-        }
-        catch (NoSuchElementException exception){
+        } catch (NoSuchElementException exception) {
             message.setText("Not found");
             return new ResponseEntity(message, HttpStatus.NOT_FOUND);
         }
     }
+
     @DeleteMapping("/blog/{id}")
     @ResponseBody
-    public ResponseEntity deleteById(@PathVariable String id){
+    public ResponseEntity deleteById(@PathVariable String id) {
         try {
             int blogId = Integer.parseInt(id);
             this.blogReprository.deleteById(blogId);
             message.setText("Blog deleted");
             return new ResponseEntity(message, HttpStatus.OK);
-        }
-        catch (EmptyResultDataAccessException exception){
+        } catch (EmptyResultDataAccessException exception) {
             message.setText("Not found");
             return new ResponseEntity(message, HttpStatus.NOT_FOUND);
         }
 
     }
+
     @PostMapping("/blog")
     @ResponseBody
-    public ResponseEntity create(@RequestBody Blog body){
-        String title=body.getTitle();
-        String content=body.getContent();
+    public ResponseEntity create(@RequestBody Blog body) {
+        String title = body.getTitle();
+        String content = body.getContent();
         this.blogReprository.save(new Blog(title, content));
         message.setText("Blog added");
         return new ResponseEntity(message, HttpStatus.OK);
